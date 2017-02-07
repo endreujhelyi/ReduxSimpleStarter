dac5cce7296a802ed18675563045c22305cb0d85
Endre <endreujhelyi@gmail.com>
2017-02-07 20:00:36 +0100

    refactoring App, YTSearch ok

--- src/index.js
+++ src/index.js
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
-
-
 const API_KEY = 'AIzaSyDZ5-Dh4LIDugbzGG0QyfqGlMqUKu28msY';
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-};
+
+class App extends Component {
+  cunstructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
 
 
 ReactDOM.render(<App />, document.querySelector('.container'));
