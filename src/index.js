0c659cdeb88182ed6b2906e6e396cf46c6db0681
Endre <endreujhelyi@gmail.com>
2017-02-07 20:16:38 +0100

    added video_list component

--- src/index.js
+++ src/index.js
@@ -3,16 +3,19 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+
+// accepting requests only from my IP address
 const API_KEY = 'AIzaSyDZ5-Dh4LIDugbzGG0QyfqGlMqUKu28msY';
 
 
 class App extends Component {
-  cunstructor(props) {
+  constructor(props) {
     super(props);
 
     this.state = { videos: [] };
 
-    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+    YTSearch({key: API_KEY, term: 'surfboards'}, videos => {
       this.setState({ videos });
     });
   }
@@ -21,6 +24,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
